baca1727f34f992caf954026debe995ae0636ca4
unknown <fillipemarquessilva@gmail.com>
2024-02-08 12:52:49 -0300

    add package.json

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ let usuariosCadastrados = [];
 const jwtSecret = 'seu_segredo_secreto';
 
 const generateToken = (usuario) => {
-  const token = jwt.sign({ usuario, exp: Math.floor(Date.now() / 1000) + 60 }, jwtSecret); // Token expira em 1 minuto
+  const token = jwt.sign({ usuario, exp: Math.floor(Date.now() / 1000) + 120 }, jwtSecret); // Token expira em 1 minuto
   return token;
 };
 
@@ -28,7 +28,7 @@ app.post('/api/login', (req, res) => {
     );
 
     if (!usuarioAutenticado) {
-      return res.status(400).json({ error: 'Credenciais inválidas.' });
+      return res.status(400).json({ error: 'Credenciais inválidas abestado.' });
     }
 
     // Gera um token JWT e o retorna
@@ -48,14 +48,14 @@ app.post('/api/cadastro', (req, res) => {
       return res.status(400).json({ error: 'Todos os campos devem ser preenchidos.' });
     }
 
-    const usuarioExistente = usuariosCadastrados.find((user) => user.email === email);
-    if (usuarioExistente) {
-      return res.status(400).json({ error: 'Email já cadastrado.' });
-    }
-
     const nomeExistente = usuariosCadastrados.find((user) => user.nome === nome);
     if (nomeExistente) {
       return res.status(400).json({ error: 'Nome já cadastrado.' });
+    }
+
+    const usuarioExistente = usuariosCadastrados.find((user) => user.email === email);
+    if (usuarioExistente) {
+      return res.status(400).json({ error: 'Email já cadastrado.' });
     }
 
     const novoUsuario = { nome, email, senha };
